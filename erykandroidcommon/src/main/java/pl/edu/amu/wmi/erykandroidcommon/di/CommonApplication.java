075f390369925b4104550ac3bf69a433542bc656
dagi12 <dagi12@o2.pl>
2017-10-21 15:27:54 +0200

    fabric, crashlytics, logging

--- erykandroidcommon/src/main/java/pl/edu/amu/wmi/erykandroidcommon/di/CommonApplication.java
+++ erykandroidcommon/src/main/java/pl/edu/amu/wmi/erykandroidcommon/di/CommonApplication.java
@@ -1,13 +1,26 @@
 package pl.edu.amu.wmi.erykandroidcommon.di;
 
 import android.support.multidex.MultiDexApplication;
+import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+
+import net.ypresto.timbertreeutils.CrashlyticsLogExceptionTree;
+import net.ypresto.timbertreeutils.CrashlyticsLogTree;
+
+import io.fabric.sdk.android.Fabric;
+import io.reactivex.plugins.RxJavaPlugins;
 import lombok.Getter;
+import timber.log.Timber;
 
 /**
  * @author Eryk Mariankowski <eryk.mariankowski@247.codes> on 21.10.17.
  */
 public abstract class CommonApplication extends MultiDexApplication {
+
+    private static final String UNHANDLED_EXCEPTION_MESSAGE = "Unhandled exception";
 
     @Getter
     private CommonApplicationComponent commonApplicationComponent;
@@ -15,9 +28,24 @@ public abstract class CommonApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        initLogging();
+        RxJavaPlugins.setErrorHandler(throwable -> Timber.e(throwable, UNHANDLED_EXCEPTION_MESSAGE));
         commonApplicationComponent = DaggerCommonApplicationComponent
                 .builder()
                 .commonApplicationModule(new CommonApplicationModule(getApplicationContext()))
                 .build();
+    }
+
+    private void initLogging() {
+        Fabric.with(this, new Crashlytics());
+        Logger.addLogAdapter(new AndroidLogAdapter());
+        Timber.plant(new Timber.DebugTree() {
+            @Override
+            protected void log(int priority, String tag, String message, Throwable t) {
+                Logger.log(priority, tag, message, t);
+            }
+        });
+        Timber.plant(new CrashlyticsLogTree(Log.DEBUG));
+        Timber.plant(new CrashlyticsLogExceptionTree());
     }
 }
