46a3896e362bf481689701e21aab834456b000a1
dagi12 <dagi12@o2.pl>
2018-01-24 14:24:32 +0100

    submodules compile

--- src/main/java/pl/edu/amu/wmi/erykandroidcommon/user/UserStore.kt
+++ src/main/java/pl/edu/amu/wmi/erykandroidcommon/user/UserStore.kt
@@ -8,14 +8,12 @@ import pl.edu.amu.wmi.erykandroidcommon.di.CommonApplication
  */
 abstract class UserStore<T : UserInterface>(private val application: CommonApplication) {
 
-    lateinit var commonApplication: CommonApplication
-
     abstract fun isSigned(): Boolean
 
     abstract var user: T?
 
     fun hardLogout() {
-        val signOutIntent = Intent(application, commonApplication.register)
+        val signOutIntent = Intent(application, application.register)
         signOutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         application.startActivity(signOutIntent)
     }
