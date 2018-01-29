5bf63a134f2d33de55349cb7818b867e1099c603
dagi12 <dagi12@o2.pl>
2018-01-29 09:51:50 +0100

    #003690 unsafe okHttpClient x

--- src/main/java/pl/edu/amu/wmi/erykandroidcommon/di/CommonApplicationModule.kt
+++ src/main/java/pl/edu/amu/wmi/erykandroidcommon/di/CommonApplicationModule.kt
@@ -10,6 +10,7 @@ import com.google.gson.GsonBuilder
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.ConnectionPool
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -17,10 +18,16 @@ import pl.edu.amu.wmi.erykandroidcommon.location.LocationService
 import pl.edu.amu.wmi.erykandroidcommon.service.PicassoCache
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 fun retroFactory(baseUrl: String, gson: Gson, okHttpClient: OkHttpClient): Retrofit =
-        Retrofit.Builder().baseUrl(baseUrl)
+    Retrofit.Builder().baseUrl(baseUrl)
         .client(okHttpClient)
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create(gson))
@@ -31,6 +38,20 @@ fun okHttpFactory(debug: Boolean, interceptor: Interceptor?): OkHttpClient {
     interceptor?.let {
         clientBuilder.addInterceptor(interceptor)
     }
+    val sslContext = SSLContext.getInstance("SSL")
+    val connectionPool = ConnectionPool(5, 60, TimeUnit.SECONDS)
+    val connectionTrustManager = arrayOf<TrustManager>(object : X509TrustManager {
+        override fun checkServerTrusted(p0: Array<out X509Certificate>?, p1: String?) = Unit
+
+        override fun checkClientTrusted(p0: Array<out X509Certificate>?, p1: String?) = Unit
+
+        override fun getAcceptedIssuers(): Array<out X509Certificate> = arrayOf()
+    })
+    sslContext.init(null, connectionTrustManager, SecureRandom())
+    clientBuilder.sslSocketFactory(sslContext.socketFactory)
+    clientBuilder.hostnameVerifier { _, _ -> true }
+
+    clientBuilder.connectionPool(connectionPool)
     if (debug) {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -38,7 +59,6 @@ fun okHttpFactory(debug: Boolean, interceptor: Interceptor?): OkHttpClient {
     }
     return clientBuilder.build()
 }
-
 
 @Module
 class CommonApplicationModule(private val application: CommonApplication) {
@@ -58,11 +78,12 @@ class CommonApplicationModule(private val application: CommonApplication) {
     @Provides
     @Singleton
     fun provideGson(): Gson = GsonBuilder()
-            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+        .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
 //            .addSerializationExclusionStrategy(object : ExclusionStrategy {
 //                override fun shouldSkipField(fieldAttributes: FieldAttributes): Boolean {
 //                    val expose: Expose = fieldAttributes.getAnnotation(Expose::class.java)
 //                    return !expose.serialize
+
 //                }
 //
 //                override fun shouldSkipClass(aClass: Class<*>): Boolean = false
@@ -75,7 +96,7 @@ class CommonApplicationModule(private val application: CommonApplication) {
 //
 //                override fun shouldSkipClass(aClass: Class<*>): Boolean = false
 //            })
-            .create()
+        .create()
 
     @Provides
     @Singleton
